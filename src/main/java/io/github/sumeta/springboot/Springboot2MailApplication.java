03a667ea65b68e1a53f1f08a5362c9679d8a03ee
tpsumeta <tpsumeta@gmail.com>
2020-03-21 16:45:20 +0700

    init

---
+++ src/main/java/io/github/sumeta/springboot/Springboot2MailApplication.java
@@ -0,0 +1,13 @@
+package io.github.sumeta.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2MailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2MailApplication.class, args);
+	}
+
+}
